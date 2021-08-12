70f1749a0848d384ff94c4eb395c6fc5133cb04a
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-08-12 21:41:12 +0200

    Add new file ison.js do the project

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -20,6 +20,21 @@ class List extends React.Component {
     static defaultProps = {
         description: settings.defaultListDescription,
     }
+    addColumn(title){
+        this.setState(state => (
+          {
+            columns: [
+              ...state.columns,
+              {
+                key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+                title,
+                icon: 'list-alt',
+                cards: []
+              }
+            ]
+          }
+        ));
+      }
     render() {
         return (
             <section className={styles.component}>
@@ -32,6 +47,9 @@ class List extends React.Component {
                     {this.state.columns.map(({ key, ...columnProps }) => (
                         <Column key={key} {...columnProps} />
                     ))}
+                </div>
+                <div className={styles.creator}>
+                    <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} />
                 </div>
             </section>
         )
