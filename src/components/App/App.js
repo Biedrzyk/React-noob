e32a8c57dddc5b8c85fe85470ed260f96a4bed9e
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-08-12 21:00:07 +0200

    Add code from the task to the app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js';
+import {pageContents, listData} from '../../data/dataStore';
 
 class App extends React.Component {
   render() {
     return (
         <main className={styles.component}>
-            <h1 className={styles.title}>My first React app</h1>
-            <h2 className={styles.subtitle}>Hello world!</h2>
-            <List title={['Things to do', <sup key='1'>very soon!</sup>]} image='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'>
-            <p>I'm planning on doing all these things sooner, rather than later! But on the other hand-if I'll do it later I can have one day off...I love procrastination!</p>
-            </List>
+            <h1 className={styles.title}>{pageContents.title}</h1>
+            <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+            <List {...listData} />
         </main>
     )
   }
