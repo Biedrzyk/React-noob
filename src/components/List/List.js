a377f7c16a9fc0f356a1f6f7a50c81d13c04f475
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-08-12 21:28:36 +0200

    Fixed problem with titles in columns

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,13 +3,13 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column';
-import {settings} from '../../data/dataStore';
+import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
     state = {
         columns: this.props.columns || [],
-      }
+    }
     static propTypes = {
         title: PropTypes.node.isRequired,
         description: PropTypes.node,
@@ -28,9 +28,9 @@ class List extends React.Component {
                     {ReactHtmlParser(this.props.description)}
                 </div>
                 <div className={styles.columns}>
-                    <Column colTitle={'ZZ Top'} />
-                    <Column colTitle={'MC Hammer'} />
-                    <Column colTitle={'Simon Webbe'} />
+                    {this.state.columns.map(({ key, ...columnProps }) => (
+                        <Column key={key} {...columnProps} />
+                    ))}
                 </div>
             </section>
         )
