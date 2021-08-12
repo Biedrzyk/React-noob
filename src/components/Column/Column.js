c76b616fac317d7f74907263a79721a5f35fe03f
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-08-12 22:02:57 +0200

    Add state in column.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card';
+import { settings } from '../../data/dataStore';
+import Icon from '../Icon/Icon';
 
 class Column extends React.Component {
+    state = {
+        cards: this.props.cards || [],
+    }
     static propTypes = {
         title: PropTypes.node.isRequired,
-        children: PropTypes.node,
-        colTitle: PropTypes.node,
     }
     render() {
         return (
             <section className={styles.component}>
                 <h3 className={styles.title}>{this.props.title}</h3>
             </section>
-        )
+        );
     }
 }
 
