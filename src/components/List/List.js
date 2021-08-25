36bd3fd4d0956e9eed9ca39380822f330ec4c024
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-08-25 21:05:37 +0200

    Add new file AppContainer

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,10 +2,10 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
-import Column from '../Column/Column';
+//import Column from '../Column/Column';
 import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
-import Creator from '../Creator/Creator';
+//import Creator from '../Creator/Creator';
 
 class List extends React.Component {
     state = {
@@ -43,14 +43,14 @@ class List extends React.Component {
           <div className={styles.description}>
             {ReactHtmlParser(this.props.description)}
           </div>
-          <div className={styles.columns}>
+          {/*<div className={styles.columns}>
             {this.state.columns.map(({ key, ...columnProps }) => (
               <Column key={key} {...columnProps} />
             ))}
           </div>
           <div className={styles.creator}>
             <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} />
-          </div>
+            </div>*/}
         </section>
       );
     }
