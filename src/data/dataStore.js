d77bff544db432596e00757758381a8bf14260f4
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-09-08 10:56:32 +0200

    Adding two new lists in the datastore.js

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -91,6 +91,18 @@ const lists = [
     description: 'Interesting things I want to check out!',
     image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
   },
+  {
+    id: 'list-2',
+    title: 'Things to do not so<sup>soon!</sup>',
+    description: 'Interesting things I want to do!',
+    image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
+  },
+  {
+    id: 'list-3',
+    title: 'Things to do <sup>when i have time!</sup>',
+    description: 'Interesting things I will do someday!',
+    image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
+  },
 ];
 
 const columns = [
